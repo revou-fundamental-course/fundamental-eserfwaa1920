7e814eb0c2e646e0f2934112651b9adf19819d7f
eserfwaa1920 <shofwa.2704@gmail.com>
2024-07-04 00:58:55 +0700

    add feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // Ini Javascript
+
+
+function validateform() {
+    let name = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let gender = document.getElementById('gender').value;
+    let notes = document.getElementById('notes').value;
+    let medsos = document.getElementById('medsos').value;
+
+    console.log (name)
+    console.log (email)
+    console.log (gender)
+    console.log (notes)
+    console.log (medsos)
+
+    if (name !=''; email !='' gender !=''|| notes !=''||medsos !='') {
+        alert ('Message succes')
+    } else {
+        alert ('Not Respon');
+    }
+
+
+}
